472e14abd5f5a7fa70cac4ac2693fe9e56a448aa
mohammed afzal <afzalmd03@gmail.com>
2023-04-29 20:46:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
-import React from 'react';
-import Header from './components/header/Header';
-import Home from './components/Home/Home';
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Projects from './components/services/Projects';
-import Qualification from './components/qualifications/Qualification';
-import Contact from './components/contact/contact';
+import "./App.css";
+import React from "react";
+import Header from "./components/header/Header";
+import Home from "./components/Home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/services/Projects";
+import Qualification from "./components/qualifications/Qualification";
+import Contact from "./components/contact/contact";
 
-const App=()=>{
-  return(
+const App = () => {
+  return (
     <>
-    <Header />
-    
-    <main className='main'>
-      <Home/>
-      <About/>
-      <Skills/>
-      <Qualification/>
-      <Projects/>
-      <Contact/>
-    </main>
-    
-    </>
+      <Header />
 
-  )
-}
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Qualification />
+        <Projects />
+        <Contact />
+      </main>
+    </>
+  );
+};
 
 export default App;
